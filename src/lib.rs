2c1e8b2496353cdd5de6b55726ad88ae00306f66
Alex Bocharov <ab@cloudflare.com>
2024-01-16 15:20:09 -0600

    Separated HyperLogLog container implementation into hyperloglog.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 //! `cardinality-estimator` is a Rust crate designed to estimate the number of distinct elements in a stream or dataset in an efficient manner.
 //!
 //! This library uses HyperLogLog++ with an optimized low memory footprint and high accuracy approach, suitable for large-scale data analysis tasks.
-mod beta;
+mod array;
 pub mod estimator;
+mod hyperloglog;
 #[cfg(feature = "with_serde")]
 mod serde;
+mod small;

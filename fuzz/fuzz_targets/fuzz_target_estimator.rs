de41103885c4dae5e0f12fe226a64b9b78998787
Alex Bocharov <ab@cloudflare.com>
2024-04-12 16:34:39 -0500

    Add fuzzing of estimator operations.

---
+++ fuzz/fuzz_targets/fuzz_target_estimator.rs
@@ -0,0 +1,31 @@
+#![no_main]
+
+use cardinality_estimator::estimator::CardinalityEstimator;
+use libfuzzer_sys::fuzz_target;
+use wyhash::wyhash;
+
+fuzz_target!(|data: &[u8]| {
+    if data.is_empty() {
+        return;
+    }
+
+    let split_index = wyhash(data, 0) as usize % data.len();
+    let (first_half, second_half) = data.split_at(split_index);
+
+    let mut estimator1 = CardinalityEstimator::<12, 6>::new();
+    for chunk in first_half.chunks(4) {
+        estimator1.insert(&chunk);
+        assert!(estimator1.estimate() > 0);
+        assert!(estimator1.size_of() > 0);
+
+    }
+
+    let mut estimator2 = CardinalityEstimator::<12, 6>::new();
+    for chunk in second_half.chunks(4) {
+        estimator2.insert(&chunk);
+        assert!(estimator2.estimate() > 0);
+        assert!(estimator2.size_of() > 0);
+    }
+
+    estimator1.merge(&estimator2);
+});
